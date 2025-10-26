35529ce876544e175047a0464ee88af45f00d8a7
AstralArtisan <1598766821@qq.com>
2025-10-26 16:14:02 +0000

    Initialize combined repo with personal and group folders

---
+++ personal/hw-list/pwords.c
@@ -0,0 +1,90 @@
+/*
+ * Word count application with one thread per input file.
+ *
+ * You may modify this file in any way you like, and are expected to modify it.
+ * Your solution must read each input file from a separate thread. We encourage
+ * you to make as few changes as necessary.
+ */
+
+/*
+ * Copyright © 2021 University of California, Berkeley
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <stddef.h>
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdlib.h>
+#include <pthread.h>
+
+#include "word_count.h"
+#include "word_helpers.h"
+
+typedef struct thread_args {
+  word_count_list_t* wclist;
+  const char* filename;
+} thread_args_t;
+
+void* thread_func(void* arg) {
+  thread_args_t* t = (thread_args_t*) arg;
+  word_count_list_t* wc = t->wclist;
+  const char* file = t->filename;
+  FILE* fp = fopen(file, "r");
+  if (fp == NULL) {
+    perror(file);
+    free(t);
+    pthread_exit(NULL);
+  }
+  count_words(wc, fp);
+  fclose(fp);
+  free(t);
+  pthread_exit(NULL);
+}
+
+/*
+ * main - handle command line, spawning one thread per file.
+ */
+int main(int argc, char* argv[]) {
+  /* Create the empty data structure. */
+  word_count_list_t word_counts;
+  init_words(&word_counts);
+
+  if (argc <= 1) {
+    /* Process stdin in a single thread. */
+    count_words(&word_counts, stdin);
+  } else {
+    int nthreads = argc - 1;
+    pthread_t threads[nthreads];
+    for (int t = 0; t < nthreads; t++) {
+      thread_args_t* args = malloc(sizeof(thread_args_t));
+      args->wclist = &word_counts;
+      args->filename = argv[t + 1];
+      int rc = pthread_create(&threads[t], NULL, thread_func, args);
+      if (rc) {
+        printf("ERROR; return code from pthread_create() is %d\n", rc);
+        exit(-1);
+      }
+    }
+    for (int i = 0; i < nthreads; i++) {
+      pthread_join(threads[i], NULL);
+    }
+  }
+
+  /* Output final result of all threads' work. */
+  wordcount_sort(&word_counts, less_count);
+  fprint_words(&word_counts, stdout);
+  pthread_exit(NULL);
+  return 0;
+}
